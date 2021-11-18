5ed16fdd37cb05cd19ca6548cf1fa9021a44c40c
kduczek <duczek.krystian@gmail.com>
2021-11-18 22:27:20 +0100

    Logic fix

--- main.py
+++ main.py
@@ -12,26 +12,21 @@ W = utils.rand_text(alphabet, 5)
 
 Xs = []
 Ys = []
-
-for _ in range(10000):
-    T += utils.rand_text(alphabet, 1)
-    counter = sunday.sundaySearch(T, W)
-    Xs.append(len(T))
-    Ys.append(counter)
-
-plt.plot(Xs, Ys)
-
 Xn = []
 Yn = []
-T = ""
-W = utils.rand_text(alphabet, 5)
+
 for _ in range(10000):
     T += utils.rand_text(alphabet, 1)
+    counterS = sunday.sundaySearch(T, W)
+    Xs.append(len(T))
+    Ys.append(counterS)
+
     naive.naiveSearch(T, W)
     counterN = naive.getCounter()
     Xn.append(len(T))
     Yn.append(counterN)
 
+plt.plot(Xs, Ys)
 plt.plot(Xn, Yn)
 plt.legend(['Sunday', 'Naiwny'], loc='upper left')
 plt.suptitle('Zmienna dlugosc tekstu przeszukiwanego')

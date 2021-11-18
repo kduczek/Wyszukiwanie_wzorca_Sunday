4ac6e2919dd83cfb188407f533f52bf43ac4303b
kduczek <duczek.krystian@gmail.com>
2021-11-18 22:37:31 +0100

    Added new plot

--- main.py
+++ main.py
@@ -1,35 +1,4 @@
-import naive
-import sunday
-import matplotlib
 import utils
 
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-T = ""
-W = utils.rand_text(alphabet, 5)
-
-Xs = []
-Ys = []
-Xn = []
-Yn = []
-
-for _ in range(10000):
-    T += utils.rand_text(alphabet, 1)
-    counterS = sunday.sundaySearch(T, W)
-    Xs.append(len(T))
-    Ys.append(counterS)
-
-    naive.naiveSearch(T, W)
-    counterN = naive.getCounter()
-    Xn.append(len(T))
-    Yn.append(counterN)
-
-plt.plot(Xs, Ys)
-plt.plot(Xn, Yn)
-plt.legend(['Sunday', 'Naiwny'], loc='upper left')
-plt.suptitle('Zmienna dlugosc tekstu przeszukiwanego')
-plt.xlabel('Dlugosc przeszukiwanego tekstu')
-plt.ylabel('Ilosc porownan')
-plt.show()
+# utils.draw_plot_various_text_lengths()
+utils.draw_plot_various_pattern_lengths()
--- utils.py
+++ utils.py
@@ -1,7 +1,73 @@
 from random import choice
+import naive
+import sunday
+import matplotlib
+
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 
 def rand_text(A, l):
     result = ""
     for _ in range(l): result += choice(A)
     return result
+
+
+def draw_plot_various_text_lengths():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    T = ""
+    W = rand_text(alphabet, 5)
+
+    Xs = []
+    Ys = []
+    Xn = []
+    Yn = []
+
+    for _ in range(10000):
+        T += rand_text(alphabet, 1)
+        counterS = sunday.sundaySearch(T, W)
+        Xs.append(len(T))
+        Ys.append(counterS)
+
+        naive.naiveSearch(T, W)
+        counterN = naive.getCounter()
+        Xn.append(len(T))
+        Yn.append(counterN)
+
+    plt.plot(Xs, Ys)
+    plt.plot(Xn, Yn)
+    plt.legend(['Sunday', 'Naiwny'], loc='upper left')
+    plt.suptitle('Zmienna dlugosc tekstu przeszukiwanego')
+    plt.xlabel('Dlugosc przeszukiwanego tekstu')
+    plt.ylabel('Ilosc porownan')
+    plt.show()
+
+
+def draw_plot_various_pattern_lengths():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    T = rand_text(alphabet, 10000)
+    W = ""
+
+    Xs = []
+    Ys = []
+    Xn = []
+    Yn = []
+
+    for _ in range(40):
+        W += rand_text(alphabet, 1)
+        counterS = sunday.sundaySearch(T, W)
+        Xs.append(len(W))
+        Ys.append(counterS)
+
+        naive.naiveSearch(T, W)
+        counterN = naive.getCounter()
+        Xn.append(len(W))
+        Yn.append(counterN)
+
+    plt.plot(Xs, Ys)
+    plt.plot(Xn, Yn)
+    plt.legend(['Sunday', 'Naiwny'], loc='center right')
+    plt.suptitle('Zmienna dlugosc wzorca')
+    plt.xlabel('Dlugosc wzorca')
+    plt.ylabel('Ilosc porownan')
+    plt.show()
